77dd12b64e6e13ea4b409d28ff28fc354ab344e0
aymurat111 <aymurat111@gmail.com>
2022-11-01 11:04:45 +0100

    exeptions

--- src/Monat_07_Exceptions/Exceptions.java
+++ src/Monat_07_Exceptions/Exceptions.java
@@ -15,13 +15,11 @@ public class Exceptions {
                 System.out.println("Bolme isleminin sonucu : "+  a/b);
             }
             System.out.println("Iyi gunler");
-        /*
-            Exception in thread "main" java.lang.ArithmeticException: / by zero
+        /* Exception in thread "main" java.lang.ArithmeticException: / by zero
         at day37_exceptions.C01_Exceptions.main(C01_Exceptions.java:15)
         Java sizin ongormediginiz bir problemle karsilasirsa
         hatanin turunu, kaynagini ve hangi satirda olustugunu hata mesaji olarak verir
-        ve calismayi durdurur
-         */
+        ve calismayi durdurur    */
         }
     }
 
