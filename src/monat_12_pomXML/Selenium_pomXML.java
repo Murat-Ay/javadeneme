fa5f14d8d05fcdaba0e6c7935ae6666b9cc015d5
aymurat111 <aymurat111@gmail.com>
2022-11-06 16:17:20 +0100

    pomXML

---
+++ src/monat_12_pomXML/Selenium_pomXML.java
@@ -0,0 +1,13 @@
+package monat_12_pomXML;
+
+public class Selenium_pomXML {
+    /*
+        <!-- https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java -->
+    <dependency>
+        <groupId>org.seleniumhq.selenium</groupId>
+        <artifactId>selenium-java</artifactId>
+        <version>4.5.0</version>
+    </dependency>
+
+     */
+}
---
+++ src/monat_12_pomXML/WebDriner_pomXML.java
@@ -0,0 +1,15 @@
+package monat_12_pomXML;
+
+public class WebDriner_pomXML {
+    /*
+        <!-- https://mvnrepository.com/artifact/io.github.bonigarcia/webdrivermanager -->
+    <dependency>
+        <groupId>io.github.bonigarcia</groupId>
+        <artifactId>webdrivermanager</artifactId>
+        <version>5.3.0</version>
+    </dependency>
+
+
+
+     */
+}
