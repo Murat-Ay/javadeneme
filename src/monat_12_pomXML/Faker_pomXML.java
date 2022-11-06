862091584ed6a505b63917190de682b28fee670b
aymurat111 <aymurat111@gmail.com>
2022-11-06 16:12:05 +0100

    pomXML

---
+++ src/monat_12_pomXML/Faker_pomXML.java
@@ -0,0 +1,10 @@
+package monat_12_pomXML;
+
+public class Faker_pomXML {
+    /*
+        <dependency>
+        <groupId>com.github.javafaker</groupId>
+        <artifactId>javafaker</artifactId>
+        <version>1.0.2</version>
+    </dependency> */
+}
---
+++ src/monat_12_pomXML/TestNG_pomXML.java
@@ -0,0 +1,14 @@
+package monat_12_pomXML;
+
+public class TestNG_pomXML {
+    /*
+        <!-- https://mvnrepository.com/artifact/org.testng/testng -->
+    <dependency>
+        <groupId>org.testng</groupId>
+        <artifactId>testng</artifactId>
+        <version>7.6.1</version>
+        <scope>test</scope>
+    </dependency>
+
+     */
+}
