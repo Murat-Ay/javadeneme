ee1fe289077eaa5d88555f1a65aa96ed8b0b1f34
aymurat111 <aymurat111@gmail.com>
2022-11-08 22:08:56 +0100

    replaceAll

--- src/monat_06_Encapsulation/gggg.java
+++
@@ -1,7 +0,0 @@
-package monat_06_Encapsulation;
-
-public class gggg {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/monat_12_pomXML/ReplaceAll.java
@@ -0,0 +1,27 @@
+package monat_12_pomXML;
+
+public class ReplaceAll {
+
+    /*
+
+\n : Starts from a new line : Metni bir alt satirdan itibaren yazdirir.
+\t  : Horizontal tab: Metni yatayda 1 tab miktar kaydirir.
+\\  : Prints a back slash    : \  Ters slash yazdirir
+\'  : Prints single quote    : '  Tek tirnak yazdirir.
+\"  : Prints double quote    : "  Cift tirnak yazdirir.
+
+
+str=str.replaceAll("\\D",""); dijit olmayan hersey
+str=str.replaceAll("\\S",""); space olmayan hersey
+str=str.replaceAll("\\W",""); harf veya rakam olmayan hersey
+str=str.replaceAll("\\d",""); dijit
+str=str.replaceAll("\\s",""); space
+str=str.replaceAll("\\w",""); harf veya rakam
+str=str.replaceAll("\\s+",""); yan yana birden fazla space
+str=str.replaceAll("\\b","");
+
+str=str.replace("3","c"); bir tane degistirir. 3=c olur.
+str=str.replaceAll("3","c"); tüm 3 leri c yapar.
+
+     */
+}
