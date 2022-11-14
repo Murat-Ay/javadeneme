43b356afbeb27c38d04f152e81390880157c79c4
aymurat111 <aymurat111@gmail.com>
2022-11-14 08:36:13 +0100

    yjjk

--- src/monat_03_Package/methodErschaffen.java
+++ src/monat_03_Package/methodErschaffen.java
@@ -2,8 +2,7 @@ package monat_03_Package;
 
 public class methodErschaffen {
     public static void main(String[] args) {
-       /* verilen 3 integer'i toplayip
-        sonucu bize donduren bir method olusturun. */
+       /* verilen 3 integer'i toplayip sonucu bize donduren bir method olusturun. */
         int sayi1=10;
         int sayi2=20;
         int sayi3=30;
@@ -11,7 +10,6 @@ public class methodErschaffen {
         System.out.println("sayilar toplami:"+ dreiSayiTopla(4,6,7)); //17
         System.out.println("sayilar toplami:"+ dreiSayiTopla(56,-65,11)); //2
     }
-
     public static int dreiSayiTopla(int sayi1, int sayi2, int sayi3){
         return sayi1+sayi2+sayi3;
     }
--- src/monat_04_Package/C05_PassByValue.java
+++ src/monat_04_Package/C05_PassByValue.java
@@ -6,7 +6,6 @@ public class C05_PassByValue {
             sayi= method1(sayi); // 10*10=100 atarsak kalici olur
             System.out.println(sayi); // 100
         }
-
         public static int  method1(int sayi){
             sayi=sayi*sayi;
             return sayi;
