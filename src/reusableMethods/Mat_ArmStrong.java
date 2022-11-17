77e026f58f62769e0a04fe61fd2a1829879c9f35
aymurat111 <aymurat111@gmail.com>
2022-11-17 08:36:33 +0100

    reusable

---
+++ src/reusableMethods/Mat_ArmStrong.java
@@ -0,0 +1,22 @@
+package reusableMethods;
+
+public class Mat_ArmStrong {
+    public static void main(String[] args) {
+        //Tüm basamaklarındaki rakamların sayı değerlerinin küpleri toplamı,
+        //kendisine eşit olan sayılara "Armstrong sayı"denir.
+        //Örneğin; 371=3*3*+7*7*7+1*1*1=27+343+1=371
+        int sum = 0;
+        int digit;
+        int temp;
+        int number = 371;
+        temp = number;
+        while (temp > 0) {
+            digit = temp % 10;
+            sum = sum + (digit * digit * digit);
+            temp = temp / 10;
+        }
+        if (number == sum){
+            System.out.println(number + " is an armstrong number");
+        }else System.out.println("it is not an armstrong number");
+    }
+}
