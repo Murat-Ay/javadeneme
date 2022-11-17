17a6850dafa2d003ab4004bddf0518a5f2f9df99
aymurat111 <aymurat111@gmail.com>
2022-11-17 13:01:12 +0100

    reusable

---
+++ src/reusableMethods/String_TersYazdir.java
@@ -0,0 +1,23 @@
+package reusableMethods;
+
+public class String_TersYazdir {
+    public static void main(String[] args) {
+
+        String str = "ey edip adanada pide ye";
+        //1.Yol :  Reverse String – For Loop
+        System.out.println("1.yol : " + reverseString(str));
+
+        //2.Yol :StringBuilder
+        String reversed = new StringBuilder(str).reverse().toString();
+        System.out.println("2.yol : " + reversed);
+        int num1=0;
+        Integer num=0;
+    }
+    public static String reverseString(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+        return reverse;
+    }
+}
---
+++ src/reusableMethods/String_UnluUnsuzSayisi.java
@@ -0,0 +1,49 @@
+package reusableMethods;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class String_UnluUnsuzSayisi {
+    public static void main(String[] args) {
+
+        //1. yol
+        String word = "Team 10 Projeye Baslayacak";
+        int sesliSayisi = 0, sessizSayisi = 0;
+        word = word.toLowerCase();
+
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i'
+                    || word.charAt(i) == 'o' || word.charAt(i) == 'u') {
+                sesliSayisi++;
+            } else if ((word.charAt(i) >= 'a'&& word.charAt(i) <= 'z')){
+                sessizSayisi++;
+            }
+        } System.out.println("number of vowels is "+sesliSayisi+" and number of consonants is "+sessizSayisi );
+
+        System.out.println(numbers(word));
+    }
+
+    // 2.yol
+    public static String numbers(String word) {
+        int countVowels = 0;
+        int countConsonants = 0;
+        List<Character> vowels = Arrays.asList('a', 'e', 'i', 'o', 'u');
+        List<Character> consonants = new ArrayList<>();
+        word=word.replaceAll("\\s","");
+        word=word.replaceAll("\\d","");
+
+        for(char i = 'a'; i <= 'z'; i++) {
+            if(!vowels.contains(i)) {
+                consonants.add(i); }
+        }
+        word = word.toLowerCase();
+        for( int i = 0; i < word.length(); i++) {
+            if(vowels.contains(word.charAt(i))) {
+                countVowels++;
+            }else {
+                countConsonants++; }
+        }
+        return "number of vowels is " + countVowels + " and number of consonants is " +
+                countConsonants; }
+}
