e4c013bb833f188fc1927e04f5e6cab4e009efbb
aymurat111 <aymurat111@gmail.com>
2022-11-17 08:21:50 +0100

    reusable

--- src/reusableMethods/DoWhileKacGunYeter.java
+++ src/reusableMethods/DoWhileKacGunYeter.java
@@ -2,16 +2,16 @@ package reusableMethods;
 
 public class DoWhileKacGunYeter {
     public static void main(String[] args) {
-        /* Adada yalnız bir maymun var Her gün 4 muz yemesi gerekiyor o adada sadece 165 muz var
-Aşağıdaki değişkenleri oluşturun ve maymunun kac gun hayatta kalabilecegini hesaplayan kodu yaziniz.
-Do while döngüsü, artırma ve eksiltme ve if deyimlerini kullanın
-int MuzSayısı =165, hayatta kalmaDays = 0;
- boolean maymunAlive = true;  */
+     /* Adada yalnız bir maymun var Her gün 4 muz yemesi gerekiyor o adada sadece 165 muz var
+     Aşağıdaki değişkenleri oluşturun ve maymunun kac gun hayatta kalabilecegini hesaplayan kodu yaziniz.
+     Do while döngüsü, artırma ve eksiltme ve if deyimlerini kullanın
+     int MuzSayısı =165, hayatta kalmaDays = 0;
+     boolean maymunAlive = true;  */
 
-//Maymunun kac gun hayatta kalacagini gosteren kod
-//Hayatta kaldigi suerece muz vermelisin
-//(DO While)
-//eger 4 ten az muz kalirsa maymunlar olur
+    //Maymunun kac gun hayatta kalacagini gosteren kod
+    //Hayatta kaldigi suerece muz vermelisin
+    //(DO While)
+    //eger 4 ten az muz kalirsa maymunlar olur
 
         int muzSayisi =165;
         boolean maymunAlive = true;
