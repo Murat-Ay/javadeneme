3329dc37d735a6e884246a48bf9b38d6de57b060
aymurat111 <aymurat111@gmail.com>
2023-04-09 15:07:29 +0200

    ap

--- src/monat_02_Package/replaceAll.java
+++ src/monat_02_Package/replaceAll.java
@@ -3,6 +3,8 @@ package monat_02_Package;
 public class replaceAll {
     public static void main(String[] args) {
 
+
+
     String  str= "java ogRe2@nMek3 #ne +Gu=zel";
     str=str.replaceAll("\\d", "");
     str=str.replaceAll("\\s", "3");
