a44945af371835d6790eeb4f195d74ca2ad66374
aymurat111 <aymurat111@gmail.com>
2023-04-16 12:54:15 +0200

    ap

--- src/monat_02_Package/replaceAll.java
+++ src/monat_02_Package/replaceAll.java
@@ -3,15 +3,19 @@ package monat_02_Package;
 public class replaceAll {
     public static void main(String[] args) {
 
+
     String  str= "java ogRe2@nMek3 #ne +Gu=zel";
     str=str.replaceAll("\\d", "");
     str=str.replaceAll("\\s", "3");
+
+
 
     System.out.println(str);
     str = str.replaceAll("\\W", "");
     str=str.replace("3", " ");
     str=str.toLowerCase();
     str=str.substring(0,1).toUpperCase() + str.substring(1)+".";
+
 
 System.out.println(str);
 }}
