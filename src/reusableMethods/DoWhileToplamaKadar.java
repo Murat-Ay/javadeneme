c72fe3a0f5fbc72bea457ad84a2de9398cb65bde
aymurat111 <aymurat111@gmail.com>
2023-05-27 23:29:42 +0200

    ap

--- src/reusableMethods/DoWhileToplamaKadar.java
+++ src/reusableMethods/DoWhileToplamaKadar.java
@@ -14,6 +14,8 @@ public class DoWhileToplamaKadar {
         int sayac = 0;
 
 
+
+
         while (sayac < 11 && toplam < 500) {
             System.out.println( "lutfen toplanmak uzere sayı giriniz" );
             sayi = imput.nextInt();
